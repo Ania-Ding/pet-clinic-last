bc52a9a284307cb8e1477d5b3195ba0be9464918
Wenling Ding <wenling368@gmail.com>
2020-05-17 00:57:13 -0400

    implement interface for services

--- pet-clinic-web/src/test/java/guru/springframework/petcliniclast/PetClinicLastApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/petcliniclast/PetClinicLastApplicationTests.java
@@ -1,12 +1,13 @@
-package petcliniclast;
+package guru.springframework.petcliniclast;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {PetClinicLastApplication.class})
 public class PetClinicLastApplicationTests {
 
     @Test
