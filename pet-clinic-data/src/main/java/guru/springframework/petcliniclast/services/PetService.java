be14241ca116e2f9cb5403cc5efe5d11fb267de8
Wenling Ding <wenling368@gmail.com>
2020-05-17 12:04:24 -0400

    refactor to common CrudService. close #11

--- pet-clinic-data/src/main/java/guru/springframework/petcliniclast/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/petcliniclast/services/PetService.java
@@ -2,13 +2,6 @@ package guru.springframework.petcliniclast.services;
 
 import guru.springframework.petcliniclast.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long>{
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
