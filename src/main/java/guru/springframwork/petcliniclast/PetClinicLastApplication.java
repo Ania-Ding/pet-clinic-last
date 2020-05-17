212305bdc3c477f5b3e3d3f5224110c35333ec97
Wenling Ding <wenling368@gmail.com>
2020-05-16 20:40:17 -0400

    Initial

---
+++ src/main/java/guru/springframwork/petcliniclast/PetClinicLastApplication.java
@@ -0,0 +1,13 @@
+package guru.springframwork.petcliniclast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetClinicLastApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetClinicLastApplication.class, args);
+    }
+
+}
