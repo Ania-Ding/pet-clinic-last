775d31011562705dd69e43f5c821c9efab2925d9
Wenling Ding <wenling368@gmail.com>
2020-05-18 15:39:31 -0400

    Add vet page and controller. closes #7

---
+++ pet-clinic-web/src/main/java/guru/springframework/petcliniclast/controller/VetController.java
@@ -0,0 +1,14 @@
+package guru.springframework.petcliniclast.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
+    public String listsVets() {
+        return "vets/index";
+    }
+}
