b7f857d02fa13284aba8b9b218e8ecae5488f25f
Wenling Ding <wenling368@gmail.com>
2020-05-18 16:02:43 -0400

    Add owner page and controller. closes #13

--- pet-clinic-web/src/main/java/guru/springframework/petcliniclast/controller/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petcliniclast/controller/IndexController.java
@@ -3,8 +3,9 @@ package guru.springframework.petcliniclast.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 @Controller
-public class indexController {
+public class IndexController {
     @RequestMapping({"", "/", "index", "index.html"})
     public String index() {
         return "index";
--- pet-clinic-web/src/main/java/guru/springframework/petcliniclast/controller/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/springframework/petcliniclast/controller/OwnerController.java
@@ -1,15 +1,15 @@
 package guru.springframework.petcliniclast.controller;
 
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping("/vets")
+@RequestMapping("/owners")
 @Controller
-public class VetController {
+public class OwnerController {
 
     @RequestMapping({"/", "/index", "/index.html"})
-    public String listsVets() {
-        return "vets/index.html";
+    public String listOwners() {
+        return "owners/index";
     }
+
 }
