8f24b8d503a7a8bdeb5978a689a67fbaa729a7cb
Wenling Ding <wenling368@gmail.com>
2020-05-19 01:45:44 -0400

    Added Speciality Map Impl

---
+++ pet-clinic-data/src/main/java/guru/springframework/petcliniclast/services/SpecialtiesService.java
@@ -0,0 +1,6 @@
+package guru.springframework.petcliniclast.services;
+
+import guru.springframework.petcliniclast.model.Speciality;
+
+public interface SpecialtiesService extends CrudService<Speciality, Long> {
+}
