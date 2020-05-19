cd4f608a751d44e498a3a85e646ec0d6587537ab
Wenling Ding <wenling368@gmail.com>
2020-05-19 00:38:19 -0400

    added PetTypeService and PetTypeMapService

---
+++ pet-clinic-data/src/main/java/guru/springframework/petcliniclast/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package guru.springframework.petcliniclast.services;
+
+import guru.springframework.petcliniclast.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
