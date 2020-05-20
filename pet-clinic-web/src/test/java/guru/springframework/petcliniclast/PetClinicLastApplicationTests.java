4c444eedd0a03d09ca7d815f3b2b4f3bdef72e05
Wenling Ding <wenling368@gmail.com>
2020-05-19 22:12:03 -0400

    upgraded to JUnit 5. closes #4

--- pet-clinic-web/src/test/java/guru/springframework/petcliniclast/PetClinicLastApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/springframework/petcliniclast/PetClinicLastApplicationTests.java
@@ -1,12 +1,11 @@
 package guru.springframework.petcliniclast;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest(classes = {PetClinicLastApplication.class})
 public class PetClinicLastApplicationTests {
 
